1a1e140f8c9e56a558fa580cb2effbd1090bc2da
root <root@ip-172-31-79-20.ec2.internal>
2025-12-10 07:31:57 +0000

    data added

--- app.java
+++ app.java
@@ -2,6 +2,11 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); // Prints "Hello, World!" to the console
     }
+
 }
 
 java sample code updated into github repository
+
+
+new code by dev1 without pull the code
+
